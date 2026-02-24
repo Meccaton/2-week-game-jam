0125379bb2e1de44e8ab2b8c911f7f863672f3d9
Meccaton <ericmecca25@gmail.com>
2026-02-23 16:53:09 -0800

    minor changes

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public int score;
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+}
