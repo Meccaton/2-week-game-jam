17f64738a08a0045a6abf4a325830a4f3122a9a2
Meccaton <ericmecca25@gmail.com>
2026-02-24 17:18:35 -0800

    you can play one round of flipping

--- Assets/Scripts/CoinFlipControllerV2.cs
+++ Assets/Scripts/CoinFlipControllerV2.cs
@@ -3,71 +3,88 @@ using System.Collections.Generic;
 
 public class CoinFlipControllerV2 : MonoBehaviour
 {
+    public bool flippingTime;
     public int state;
-    public bool movingUp;
-    public Vector3 bottom;
-    public Vector3 top;
+    //public bool movingUp;
+    //public Vector3 bottom;
+    //public Vector3 top;
     public GameObject cam;
-    public Dictionary<string, int> chances;
-    public string topFace = "Heads";
-    public string botFace = "Tails";
-    public Transform displayFace;
-    public Transform previousFace;
+    //public Dictionary<string, int> chances;
+    public List<CoinBehavior> coins;
+    public int coinIdx;
+    //public string topFace = "Heads";
+    //public string botFace = "Tails";
+    //public Transform displayFace;
+    //public Transform previousFace;
+    public List<string> winners;
 
     void Start()
     {
+        flippingTime = false;
         state = 0;
 
-        movingUp = true;
+        //movingUp = true;
 
-        bottom = new Vector3(0f, .5f, 0f);
-        top = new Vector3(0f, 2f, 0f);
+        //bottom = new Vector3(0f, .5f, 0f);
+        //top = new Vector3(0f, 2f, 0f);
 
-        chances = new Dictionary<string, int>();
-        chances.Add("Blank", 0);   // placeholder; shouldnt ever show up
-        chances.Add("Heads", 50);  // +1 score
-        chances.Add("Tails", 50);  // 0 score
-        chances.Add("Circle", 0);  // 
-        chances.Add("Swords", 0);  // 
-        chances.Add("Crown", 0);   // 
-        chances.Add("Sun", 0);     // 
-        chances.Add("Diamond", 0); // 
-        chances.Add("Heart", 0);   // 
-        chances.Add("Lighting", 0);// 
-        chances.Add("Shield", 0);  // 
-        chances.Add("Square", 0);  // 
-        chances.Add("Rhombus", 0); // 
-        chances.Add("Star", 0);    // 
-        chances.Add("Triangle", 0);// 
+        coinIdx = 0;
 
-        displayFace = transform.Find(topFace);
-        displayFace.gameObject.SetActive(true);
-        previousFace = transform.Find(botFace);
-        previousFace.gameObject.SetActive(true);
-        previousFace.localPosition = new Vector3(0f, .09f, 0f);
-        previousFace.rotation = Quaternion.Euler(90f, 0f, 0f);
+        winners = new List<string>(3)
+        {
+            null,
+            null,
+            null
+        };
+
+        //chances = new Dictionary<string, int>();
+        //chances.Add("Blank", 0);   // placeholder; shouldnt ever show up
+        //chances.Add("Heads", 50);  // +1 score
+        //chances.Add("Tails", 50);  // 0 score
+        //chances.Add("Circle", 0);  // 
+        //chances.Add("Swords", 0);  // 
+        //chances.Add("Crown", 0);   // 
+        //chances.Add("Sun", 0);     // 
+        //chances.Add("Diamond", 0); // 
+        //chances.Add("Heart", 0);   // 
+        //chances.Add("Lighting", 0);// 
+        //chances.Add("Shield", 0);  // 
+        //chances.Add("Square", 0);  // 
+        //chances.Add("Rhombus", 0); // 
+        //chances.Add("Star", 0);    // 
+        //chances.Add("Triangle", 0);// 
+
+        //displayFace = transform.Find(topFace);
+        //displayFace.gameObject.SetActive(true);
+        //previousFace = transform.Find(botFace);
+        //previousFace.gameObject.SetActive(true);
+        //previousFace.localPosition = new Vector3(0f, .09f, 0f);
+        //previousFace.rotation = Quaternion.Euler(90f, 0f, 0f);
     }
 
     void Update()
     {
-        cam.transform.LookAt(transform);
-        cam.transform.rotation *= Quaternion.Euler(-20f, 0f, 0f);
+        if(flippingTime)
+        {
+            cam.transform.LookAt(coins[coinIdx].transform);
+            cam.transform.rotation *= Quaternion.Euler(-15f, 0f, 0f);
 
-        if (state == 0)
-        {
-            Hold();
-        }
-        else if (state == 1)
-        {
-            Flip();
-        }
-        else if (state == 2)
-        {
-            GetOutcome();
-        }
-        else if (state == 3)
-        {
-            WaitForInput();
+            if (state == 0)
+            {
+                Hold();
+            }
+            else if (state == 1)
+            {
+                Flip();
+            }
+            else if (state == 2)
+            {
+                GetOutcome();
+            }
+            else if (state == 3)
+            {
+                WaitForInput();
+            }
         }
     }
 
@@ -75,9 +92,10 @@ public class CoinFlipControllerV2 : MonoBehaviour
     {
         //cam.transform.position = Vector3.Lerp(cam.transform.position, originalPos, 2 * Time.deltaTime);
 
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            movingUp = true;
+            winners[coinIdx] = null;
+            coins[coinIdx].movingUp = true;
             
             state = 1;
         }
@@ -85,84 +103,84 @@ public class CoinFlipControllerV2 : MonoBehaviour
 
     void Flip()
     {
-        if (movingUp)
+        if (coins[coinIdx].FlipAnimation())
         {
-            transform.position = Vector3.MoveTowards(transform.position, top, 2f * Time.deltaTime);
-
-            if (transform.position.y >= 2f)
-            {
-                movingUp = false;
-            }
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, bottom, 2f * Time.deltaTime);
-        }
-
-        transform.Rotate(3000f * Time.deltaTime, 0, 0);
-
-        if (transform.position.y <= .5f && !movingUp)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             state = 2;
         }
     }
 
     void GetOutcome()
     {
-        int total = 0;
-        foreach(KeyValuePair<string, int> entry in chances)
+        winners[coinIdx] = coins[coinIdx].CalculateOutcome();
+        if (winners[coinIdx] != null)
         {
-            total += entry.Value;
+            state = 3;
         }
-        int rando = Random.Range(0, total);
-        int runningTotal = 0;
-        foreach(KeyValuePair<string, int> entry in chances)
-        {
-            runningTotal += entry.Value;
-            if(rando < runningTotal)
-            {
-                DisplayFace(entry.Key);
-                state = 3;
-                break;
-            }
-        }
+
+        //if (coins[coinIdx].CalculateOutcome())
+        //{
+        //    state = 3;
+        //}
+
+        //int total = 0;
+        //foreach(KeyValuePair<string, int> entry in chances)
+        //{
+        //    total += entry.Value;
+        //}
+        //int rando = Random.Range(0, total);
+        //int runningTotal = 0;
+        //foreach(KeyValuePair<string, int> entry in chances)
+        //{
+        //    runningTotal += entry.Value;
+        //    if(rando < runningTotal)
+        //    {
+        //        DisplayFace(entry.Key);
+        //        state = 3;
+        //        break;
+        //    }
+        //}
     }
 
     void WaitForInput()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //displayFace.gameObject.SetActive(false);
+            coinIdx++;
+            if(coinIdx > 2)
+            {
+                coinIdx = 0;
+                flippingTime = false;
+            }
             state = 0;
         }
     }
 
-    void DisplayFace(string faceName)
-    {
+    //void DisplayFace(string faceName)
+    //{
         
 
-        if (faceName != topFace)
-        {
-            previousFace.gameObject.SetActive(false);
-            previousFace.localPosition = new Vector3(0f, .1f, 0f);
-            previousFace.rotation = Quaternion.Euler(-90f, 180f, 0f);
+    //    if (faceName != topFace)
+    //    {
+    //        previousFace.gameObject.SetActive(false);
+    //        previousFace.localPosition = new Vector3(0f, .1f, 0f);
+    //        previousFace.rotation = Quaternion.Euler(-90f, 180f, 0f);
 
-            botFace = topFace;
-            previousFace = transform.Find(botFace);
-            previousFace.localPosition = new Vector3(0f, .09f, 0f);
-            previousFace.rotation = Quaternion.Euler(90f, 0f, 0f);
+    //        botFace = topFace;
+    //        previousFace = transform.Find(botFace);
+    //        previousFace.localPosition = new Vector3(0f, .09f, 0f);
+    //        previousFace.rotation = Quaternion.Euler(90f, 0f, 0f);
 
-            topFace = faceName;
+    //        topFace = faceName;
 
-            //displayFace.gameObject.SetActive(false);
+    //        //displayFace.gameObject.SetActive(false);
 
-            displayFace = transform.Find(topFace);
-            displayFace.gameObject.SetActive(true);
-            Debug.Log("displayFace = " + displayFace);
-            Debug.Log("previousFace = " + previousFace);
-        }
+    //        displayFace = transform.Find(topFace);
+    //        displayFace.gameObject.SetActive(true);
+    //        Debug.Log("displayFace = " + displayFace);
+    //        Debug.Log("previousFace = " + previousFace);
+    //    }
 
         
-    }
+    //}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,31 +1,65 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class GameController : MonoBehaviour
 {
     public int state;
+    public CoinFlipControllerV2 cfc;
+    public List<string> playerCoins;
+    public List<string> opponentCoins;
 
     void Start()
     {
         state = 0;
+        playerCoins = new List<string>(3)
+        {
+            null,
+            null,
+            null
+        };
+        opponentCoins = new List<string>(3)
+            {
+            null,
+            null,
+            null
+        };
     }
 
     void Update()
     {
         if(state == 0) // new character walks in
         {
-
+            // TODO
+            state = 1;
         }
         else if(state == 1) // character dialogue
         {
-
+            // TODO
+            state = 2;
         }
         else if(state == 2) // coin choose phase
         {
             // TODO
+            state = 3;
         }
         else if(state == 3) // coin flip phase
         {
-
+            cfc.flippingTime = true;
+            state = 4;
+        }
+        else if(state == 4)
+        {
+            if (!cfc.flippingTime) // wait for flipping to complete
+            {
+                state = 5;
+            }
+        }
+        else if (state == 5)
+        {
+            //cfc.winners;
+            Debug.Log("Made it to post flipping state");
         }
     }
 }
