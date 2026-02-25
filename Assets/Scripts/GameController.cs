5e1c3e0ad23ba52aedf646df23e4176d8007a49c
Meccaton <ericmecca25@gmail.com>
2026-02-24 19:39:36 -0800

    turn progression

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,12 @@ using System.Collections.Generic;
 public class GameController : MonoBehaviour
 {
     public int state;
+    public int playerScore;
+    public int opponentScore;
+    public int round;
+    public int maxRounds;
+    public int playerRecord;
+    public int opponentRecord;
     public CoinFlipControllerV2 cfc;
     public List<string> playerCoins;
     public List<string> opponentCoins;
@@ -13,6 +19,12 @@ public class GameController : MonoBehaviour
     void Start()
     {
         state = 0;
+        playerScore = 0;
+        opponentScore = 0;
+        round = 0;
+        playerRecord = 0;
+        opponentRecord = 0;
+
         playerCoins = new List<string>(3)
         {
             null,
@@ -56,12 +68,71 @@ public class GameController : MonoBehaviour
                 state = 5;
             }
         }
-        else if (state == 5)
+        else if (state == 5) // acquire coin data, determine winner
         {
+            playerScore = 0;
+            opponentScore = 0;
+
             playerCoins = cfc.winners;
             opponentCoins = cfc.opponentWinners;
-            Debug.Log("Player coins: " + playerCoins[0] + ", " + playerCoins[1] + ", " + playerCoins[2]);
-            Debug.Log("Opponent coins: " + opponentCoins[0] + ", " + opponentCoins[1] + ", " + opponentCoins[2]);
+            
+            foreach(string c in playerCoins)
+            {
+                if(c == "Heads")
+                {
+                    playerScore++;
+                }
+            }
+
+            foreach(string c in opponentCoins)
+            {
+                if(c == "Heads")
+                {
+                    opponentScore++;
+                }
+            }
+
+            if(playerScore > opponentScore)
+            {
+                playerRecord++;
+                round++;
+                state = 3;
+                Debug.Log("Player won this round");
+            }
+            else if(opponentScore > playerScore)
+            {
+                opponentRecord++;
+                round++;
+                state = 3;
+                Debug.Log("Opponent won this round");
+            }
+            else
+            {
+                state = 3;
+                Debug.Log("Draw; replaying this round");
+            }
+
+            if (round >= maxRounds)
+            {
+                if (playerScore > opponentScore)
+                {
+                    state = 6;
+                    Debug.Log("Player wins");
+                }
+                else if (opponentScore > playerScore)
+                {
+                    state = 6;
+                    Debug.Log("Opponent wins");
+                }
+                else
+                {
+                    Debug.Log("How did you get here?");
+                }
+            }
+        }
+        else if(state == 6)
+        {
+
         }
     }
 }
